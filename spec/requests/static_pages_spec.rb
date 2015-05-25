b2ff40bcd9c249a043f0ca89b2df3bfdd02dd7f0
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-05-25 15:39:48 +0900

    Practice

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,57 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+	let(:title) { "Rails/" }
 
-    it "should have the content 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Home')
-    end
+  	describe "Home page" do
 
-    it "should have ther right title" do
+    	it "should have the content 'Home'" do
+      	visit '/static_pages/home'
+      	expect(page).to have_content('Home')
+    	end
+
+    	it "should have ther right title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Home")
-    end
-  end
+    	expect(page).to have_title("#{title}Home")
+    	end
+  	end
 
-  describe "Help page" do
+  	describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-it "should have ther right title" do
+    	it "should have the content 'Help'" do
+      	visit '/static_pages/help'
+      	expect(page).to have_content('Help')
+    	end
+		
+		it "should have ther right title" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("Help")
-    end
+    	expect(page).to have_title("#{title}Help")
+    	end
+  	end
 
-  end
+  	describe "About page" do
 
-  describe "About page" do
+    	it "should have the content 'About'" do
+      	visit '/static_pages/about'
+      	expect(page).to have_content('About')
+    	end
 
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
-
-    it "should have ther right title" do
+    	it "should have ther right title" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("About")
-    end
-  end
+    	expect(page).to have_title("#{title}About")
+    	end
+   	end
+
+  	describe "Contact page" do
+
+    	it "should have the content 'Contact'" do
+      	visit '/static_pages/contact'
+      	expect(page).to have_content('Contact')
+    	end
+
+    	it "should have ther right title" do
+    	visit '/static_pages/contact'
+    	expect(page).to have_title("#{title}Contact")
+    	end
+  	end
 end
