2a66c7aadc4ad9258c0ac4f055e7b8c538743707
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-05-29 14:04:55 +0900

    fin to 7.1

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Sample32::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # bcypy'のコスト関数を下げることでテストの速度を向上させる
+  ActiveModel::SecurePassword.min_cost = true
 end
