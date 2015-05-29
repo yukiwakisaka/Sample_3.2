1b388e74158d2248c104ee7104328f16178d86d2
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-05-29 16:59:12 +0900

    onetime save

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, 
 			format:{with: VALID_EMAIL_REGEX}, 
 			uniqueness: {case_sensitive: false}
+			
 	validates :password, length: {minimum: 6}
 end
