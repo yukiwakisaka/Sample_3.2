8bdcb8fad212111a8bea9cc621e10f58185a9642
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-05-30 00:18:51 +0900

    Fin section 7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
 
-	def gravator_for(user)
+	def gravator_for(user, options = {size:100})
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+	    size = options[:size]
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 	    image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
 
