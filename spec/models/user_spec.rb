f523313d6b4259e18fb1148573ce20fdca550f29
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-05-29 10:15:51 +0900

    fin to 6.3.1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ describe User do
 
   it{should respond_to(:name)}
   it{should respond_to(:email)}
+  it{should respond_to(:password_digest)}
 
   it {should be_valid}
 
@@ -36,7 +37,7 @@ describe User do
   	end
   end
 
-   describe "when email format is valid" do
+  describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
@@ -44,6 +45,25 @@ describe User do
         expect(@user).to be_valid
       end
     end
+  end
+
+  describe "when email address is already taken" do
+  	before do
+  		user_with_same_email = @user.dup
+  		user_with_same_email.save
+  	end
+
+  	it { should_not be_valid}
+  end  
+
+  describe "when email address is already taken" do
+  	before do
+  		user_with_same_email = @user.dup
+  		user_with_same_email.email = @user.email.upcase
+  		user_with_same_email.save
+  	end
+
+  	it { should_not be_valid}
   end
 
 end
