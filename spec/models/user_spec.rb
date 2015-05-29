c5b5d6bcae1d4cecbd0a99035267bfb1ba441a92
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-05-29 10:26:36 +0900

    fin to 6.3.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe User do
   # pending "add some examples to (or delete) #{__FILE__}"
-  before{@user = User.new(name: "Example User", email: "user@example.com")}
+  before{@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")}
 
   subject {@user}
 
   it{should respond_to(:name)}
   it{should respond_to(:email)}
   it{should respond_to(:password_digest)}
+  it{should respond_to(:password)}
+  it{should respond_to(:password_confirmation)}
 
   it {should be_valid}
 
@@ -63,6 +65,18 @@ describe User do
   		user_with_same_email.save
   	end
 
+  	it { should_not be_valid}
+  end
+
+  describe "when password is not present" do
+  	before do
+  		@user = User.new(name: "Example User", email: "user@example.com", password: "", password_confirmation: "")
+  	end
+  	it{should_not be_valid}
+  end
+
+  describe " when password doesnt match confirmation" do
+  	before { @user.password_confirmation = "mismatch"}
   	it { should_not be_valid}
   end
 
