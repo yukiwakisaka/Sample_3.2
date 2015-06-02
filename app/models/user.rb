da3de92204830b6bec600500cd42a12bf8dd9c78
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-06-02 11:05:12 +0900

    finto 9.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 
 	has_secure_password
-	before_save { self.email = email.downcase }
-	# before_save{email.downcase!}
+	# before_save { self.email = email.downcase }
+	before_save{email.downcase!}
 	before_create :create_remember_token
 
 	validates :name, presence: true, length:{maximum: 50}
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -88,6 +88,11 @@ describe "AuthenticationPages" do
 					before {patch user_path(user)}
 					specify {expect(response).to redirect_to(signin_path) }
 				end
+
+				describe "visiting the user index" do
+					before {visit users_path}
+					it { should have_title ('Sign in')}
+				end
 			end
 		end
 
@@ -108,4 +113,16 @@ describe "AuthenticationPages" do
 	      	end
 	    end
 	end
+
+	describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+  end
 end
