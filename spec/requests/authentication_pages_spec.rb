8ab79f1eeca1d85ccd490886ceb499320990a4f1
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-06-04 22:53:25 +0900

    finto 11.2.4

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -72,7 +72,6 @@ describe "AuthenticationPages" do
 					before {delete micropost_path(FactoryGirl.create(:micropost))}
 					specify { expect(response).to redirect_to(signin_path)}
 				end
-
 			end
 
 			describe "when attempting to visit a protected page" do
@@ -84,7 +83,6 @@ describe "AuthenticationPages" do
 		        end
 
 		        describe "after signing in" do
-
 		          it "should render the desired protected page" do
 		            expect(page).to have_title('Edit user')
 		          end
@@ -107,7 +105,29 @@ describe "AuthenticationPages" do
 					before {visit users_path}
 					it { should have_title ('Sign in')}
 				end
+
+				describe "visiting the following page" do
+					before {visit following_user_path(user)}
+					it { should have_title ('Sign in')}
+				end
+
+				describe "visiting the following page" do
+					before {visit followers_user_path(user)}
+					it { should have_title ('Sign in')}
+				end
 			end
+
+			describe "in the Relationships controller" do
+        		describe "submitting to the create action" do
+          			before { post relationships_path }
+          			specify { expect(response).to redirect_to(signin_path) }
+        		end
+
+        		describe "submitting to the destroy action" do
+          			before { delete relationship_path(1) }
+          			specify { expect(response).to redirect_to(signin_path) }
+        		end
+      		end
 		end
 
 		describe "as wrong user" do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,8 +39,8 @@ describe "Static pages" do
                     visit root_path
                 end
 
-                it { should have_link ("0 following", href: following_user_path(user))}
-                it { should have_link ("1 followers", href: followers_user_path(user))}
+                it { should have_link("0 following", href: following_user_path(user))}
+                it { should have_link("1 followers", href: followers_user_path(user))}
             end
         end
 	end
